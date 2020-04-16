691954fe6d7ccc327c0c31f3a416a47c9a39ddda
EEZH <54198538+EEZH@users.noreply.github.com>
2020-04-16 11:30:44 +0500

    delete useless code

--- parsing.py
+++ parsing.py
@@ -26,15 +26,21 @@ def get_pages_count(html):
         return 1
     # print(pagination)
 
+def save_file(objects, path):
+    with open(path, 'w', newline="", encoding='utf-16') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['Описание', 'Ссылка', 'Цена', 'Адрес', 'Дата', 'Телефон'])
+        for item in objects:
+            writer.writerow([item['title'], item['link'], item['price'], item['adress'], item['date'], item['phone']])
+
+
 
 def get_content(html):
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('div', class_='item__line')
     objects = []
-
-
+    n = 0
     for item in items:
-        # n = 0
         phone_agent = Bot()
         objects.append({
             'title': item.find('div', class_='snippet-title-row').get_text(strip=True),
@@ -44,35 +50,32 @@ def get_content(html):
             'date': item.find('div', class_='snippet-date-info').get_text(strip=True),
             'phone': phone_agent.navigate(HOST + item.find('a', class_='snippet-link').get('href')),
         })
-        # n+=1
-        # print(n)
+        n+=1
+
+        # save_file(objects, OBJECTS)
         print(objects)
+    print(n)
     print(objects)
     print(len(objects))
 
     return objects
 
 
-def save_file(objects, path):
-    with open(path, 'w', newline="", encoding='utf-16') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow(['Описание', 'Ссылка', 'Цена', 'Адрес', 'Дата'])
-        for item in objects:
-            writer.writerow([item['title'], item['link'], item['price'], item['adress'], item['date']])
+
 
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
         get_content(html.text)
         objects = []
-        # pages_count = get_pages_count(html.text)
-        # for page in range(1, pages_count + 1):
-        #     print(f'Парсинг страницы {page} из {pages_count}...')
-        #     html = get_html(URL, params={'p': page})
-        #     objects.extend((get_content(html.text)))
-        # save_file(objects, OBJECTS)
-        # print(objects)
-        # print(f"Получено {len(objects)} объектов")
+        pages_count = get_pages_count(html.text)
+        for page in range(1, 2):
+            print(f'Парсинг страницы {page} из {pages_count}...')
+            html = get_html(URL, params={'p': page})
+            objects.extend((get_content(html.text)))
+        save_file(objects, OBJECTS)
+        print(objects)
+        print(f"Получено {len(objects)} объектов")
     else:
         print("Error!")
 
